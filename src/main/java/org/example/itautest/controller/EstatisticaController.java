d748f930176ac8f958d872659d7363d18e01d507
DevNoahF <noahvf16@outlook.com>
2026-01-29 21:08:07 -0300

    create: controller estatisstica get for listarEstatisticas

---
+++ src/main/java/org/example/itautest/controller/EstatisticaController.java
@@ -0,0 +1,26 @@
+package org.example.itautest.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.itautest.dto.EstatisticaDTO;
+import org.example.itautest.service.EstatisticaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/estatistica")
+@RequiredArgsConstructor
+public class EstatisticaController {
+
+    private final EstatisticaService estatisticaService;
+
+
+    @GetMapping
+    public ResponseEntity<EstatisticaDTO> lisarEstatisicas (
+            @RequestParam(value = "tempoBuscado", required = false, defaultValue = "60") Integer tempoBuscado){
+
+        return ResponseEntity.ok(estatisticaService.calcularEstatisticaTransacao(tempoBuscado));
+    }
+}
